4622282374cd176d2e74185934ce79ad595147fa
Ben-21 <bs@schaefer-inet.de>
2023-09-28 16:54:16 +0200

    add empty test for BackenApplication

---
+++ backend/src/test/java/com/bssoftwaredevelopment/backend/main/BackenApplicationTest.java
@@ -0,0 +1,13 @@
+package com.bssoftwaredevelopment.backend.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackenApplicationTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
