206a2924cbe4758a15cac8726af26d9bfe27a2a2
Ben-21 <bs@schaefer-inet.de>
2023-09-28 17:55:45 +0200

    exlude BackenApplication from jacoco

---
+++ backend/src/test/java/com/bssoftwaredevelopment/backend/mainapp/BackendApplicationTest.java
@@ -0,0 +1,14 @@
+package com.bssoftwaredevelopment.backend.mainapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BackendApplicationTest {
+    @Test
+    void testMain(){
+
+    }
+
+}
