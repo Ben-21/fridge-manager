d661de5ec9432c6a04b88aabaa245fce6acd7259
Ben-21 <bs@schaefer-inet.de>
2023-09-30 15:57:26 +0200

    setup webclient get product

--- backend/src/main/java/com/bssoftwaredevelopment/backend/models/OpenFoodFactsProduct.java
+++ backend/src/main/java/com/bssoftwaredevelopment/backend/models/OpenFoodFactsProduct.java
@@ -1,7 +1,7 @@
 package com.bssoftwaredevelopment.backend.models;
 
 public record OpenFoodFactsProduct(
-        String id,
+        String _id,
         String product_name,
         String image_url,
         String quantity
