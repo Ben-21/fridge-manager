d02fe0a915a304f7fffc07f621fe986f50114345
Ben-21 <bs@schaefer-inet.de>
2023-10-02 09:45:51 +0200

    add repo, service, controller

---
+++ backend/src/main/java/com/bssoftwaredevelopment/backend/FridgeManagerRepo.java
@@ -0,0 +1,9 @@
+package com.bssoftwaredevelopment.backend;
+
+import com.bssoftwaredevelopment.backend.models.Item;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FridgeManagerRepo extends MongoRepository<Item, String> {
+}
---
+++ backend/src/main/java/com/bssoftwaredevelopment/backend/FridgeManagerService.java
@@ -0,0 +1,7 @@
+package com.bssoftwaredevelopment.backend;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FridgeManagerService {
+}
