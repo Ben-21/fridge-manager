4472c668cbbe1b8ffb76783800636d5cd5552d37
Ben-21 <bs@schaefer-inet.de>
2023-10-04 09:34:04 +0200

    make barcode scannable and return product name

--- frontend/src/components/FetchOpenFoodFactsItem.tsx
+++ frontend/src/components/FetchOpenFoodFactsItem.tsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import React, {useEffect, useState} from "react";
 import axios from "axios";
 import {OpenFoodFactsItem} from "../models/models.ts";
 
@@ -8,31 +8,43 @@ export default function FetchOpenFoodFactsItem() {
 
     const [openFoodFactsItem, setOpenFoodFactsItem] = useState<OpenFoodFactsItem | undefined>(undefined);
     const [barcode, setBarcode] = useState<string>("");
+    const [name, setName] = useState<string>("");
 
 
-   function fetchOpenFoodFactsItem(barcode: string) {
-       axios
-           .get("/api/items/openfoodapi/" + barcode)
-           .then((response) => response.data)
-              .then((data) => {
-                  setOpenFoodFactsItem(data);
-              })
-           .catch(console.error)
-   }
+    useEffect(() => {
+        if (openFoodFactsItem) {
+            setName(openFoodFactsItem.product.product_name);
+        }
+    }, [openFoodFactsItem])
 
+    function fetchOpenFoodFactsItem(barcode: string) {
+
+        axios
+            .get("/api/items/openfoodapi/" + barcode)
+            .then((response) => response.data)
+            .then((data) => {
+                setOpenFoodFactsItem(data);
+            })
+            .catch(console.error)
+    }
+
+
+    function handleNameChange(event: React.ChangeEvent<HTMLInputElement>) {
+        setName(event.target.value)
+    }
 
     return (
         <>
             <form id="barcodeForm">
+                Barcode:
                 <input name="barcode" type="text" value={barcode} onChange={(event) => setBarcode(event.target.value)}/>
-                <button type="submit" onClick={() => fetchOpenFoodFactsItem(barcode)}>Search</button>
+                <button type={"button"} onClick={() => fetchOpenFoodFactsItem(barcode)}>Search</button>
             </form>
 
             <form id="productForm">
-                <input name="name" type="text" value={openFoodFactsItem?.product.product_name}>
-
-                </input>
+                Product Name:
+                <input name="name" type="text" value={name} onChange={handleNameChange} style={{width: "550px"}}/>
             </form>
         </>
-)
+    )
 }
