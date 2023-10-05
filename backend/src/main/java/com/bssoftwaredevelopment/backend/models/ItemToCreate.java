a385d7773fe90cd0e6f4c2436764ecc3e9a86fe2
Ben-21 <bs@schaefer-inet.de>
2023-10-05 17:32:51 +0200

    add barcode to model

--- backend/src/main/java/com/bssoftwaredevelopment/backend/models/ItemToCreate.java
+++ backend/src/main/java/com/bssoftwaredevelopment/backend/models/ItemToCreate.java
@@ -4,6 +4,7 @@ package com.bssoftwaredevelopment.backend.models;
 
 public record ItemToCreate(
         String name,
+        String barcode,
         String imageUrl,
         StorageLocation storageLocation,
         int stockAmount,
