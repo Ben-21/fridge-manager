ebb2055de8d83492465c4882531b24ea507f6810
Ben-21 <bs@schaefer-inet.de>
2023-10-05 17:58:59 +0200

    add toast container to app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css'
 import AddEditItem from "./pages/AddEditItem.tsx";
+import {ToastContainer} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 
 
     return (
         <>
+            <ToastContainer/>
             <AddEditItem/>
         </>
     )
