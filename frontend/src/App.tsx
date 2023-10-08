a87081df16974acd060de31957616d01166c440d
Ben-21 <bs@schaefer-inet.de>
2023-10-08 15:53:25 +0200

    remove testItem

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,36 @@
 import './App.css'
-import {ToastContainer} from "react-toastify";
+import {toast, ToastContainer} from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import {Item, StockUnit, StorageLocation} from "./models/models.ts";
 import ItemCard from "./components/ItemCard.tsx";
 import FetchItem from "./components/FetchItem.tsx";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 function App() {
 
     const [barcode, setBarcode] = useState<string>("");
     const [item, setItem] = useState<Item>();
-    const testItem: Item = {
-        id: "0123",
-        barcode: "0000",
-        name: "Test Item",
-        imageUrl: "https://miro.medium.com/v2/da:true/resize:fit:800/1*nzx2zFmbdZ5xWUQHWohvkw.gif",
-        storageLocation: StorageLocation.FRIDGE,
-        stockAmount: 10,
-        warnStockAmount: 2,
-        stockUnit: StockUnit.PIECE,
-        quantity: "150 g"
-    };
+    const [items, setItems] = useState<Item[]>();
+
+
+    useEffect(()=>{
+        fetchAllItems();
+    },[])
+
+    function fetchAllItems() {
+        axios
+            .get("/api/items")
+            .then((response) => response.data)
+            .then((data) => {
+                setItems(data)
+            })
+            .catch((error) => {
+                toast.error("Something went wrong trying to fetch all items");
+                console.log(error);
+            })
+
+    }
 
     const fetchItem = (childData: Item) => {
         setItem(childData);
@@ -33,6 +43,7 @@ function App() {
             <input name={"barcode"} type={"text"} value={barcode} onChange={(event) => setBarcode(event.target.value)}/>
             <FetchItem fetchData={fetchItem} barcode={barcode}/>
             {item && <ItemCard item={item}/>}
+
         </>
     )
 }
