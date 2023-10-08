2f9274d7fb6589da04761a3d24101155ea9156f7
Ben-21 <bs@schaefer-inet.de>
2023-10-08 15:49:47 +0200

    remove update funtions for the moment

--- backend/src/main/java/com/bssoftwaredevelopment/backend/customexceptions/ItemByIdNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.bssoftwaredevelopment.backend.customexceptions;
-
-public class ItemByIdNotFoundException extends RuntimeException{
-//    public ItemByIdNotFoundException(String id){
-//        super("Could not find item with id: " + id);
-//    }
-}
