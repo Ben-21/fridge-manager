ab898013868a81439db9c80bb222bfffede1fdd0
Ben-21 <bs@schaefer-inet.de>
2023-10-08 15:28:12 +0200

    add fieldset for ItemCard.tsx

--- frontend/src/components/ItemCard.tsx
+++ frontend/src/components/ItemCard.tsx
@@ -7,32 +7,35 @@ type Props = {
 export default function ItemCard(props: Props) {
     return (
         <div className="item-card">
-            <div className="image-container">
-                <img src={props.item.imageUrl} alt="Product Image" />
-            </div>
-            <div className="details">
-                <div>
-                    <strong>Barcode:</strong> {props.item.barcode}
+            <fieldset>
+                <legend>Product Details</legend>
+                <div className="image-container">
+                    <img src={props.item.imageUrl} alt="Product Image" />
                 </div>
-                <div>
-                    <strong>Name:</strong> {props.item.name}
+                <div className="details">
+                    <div>
+                        <strong>Barcode:</strong> {props.item.barcode}
+                    </div>
+                    <div>
+                        <strong>Name:</strong> {props.item.name}
+                    </div>
+                    <div>
+                        <strong>Quantity:</strong> {props.item.quantity}
+                    </div>
+                    <div>
+                        <strong>Storage:</strong> {props.item.storageLocation}
+                    </div>
+                    <div>
+                        <strong>In Stock:</strong> {props.item.stockAmount}
+                    </div>
+                    <div>
+                        <strong>Stock Unit:</strong> {props.item.stockUnit}
+                    </div>
+                    <div>
+                        <strong>Warn Stock Amount:</strong> {props.item.warnStockAmount}
+                    </div>
                 </div>
-                <div>
-                    <strong>Quantity:</strong> {props.item.quantity}
-                </div>
-                <div>
-                    <strong>Storage:</strong> {props.item.storageLocation}
-                </div>
-                <div>
-                    <strong>In Stock:</strong> {props.item.stockAmount}
-                </div>
-                <div>
-                    <strong>Stock Unit:</strong> {props.item.stockUnit}
-                </div>
-                <div>
-                    <strong>Warn Stock Amount:</strong> {props.item.warnStockAmount}
-                </div>
-            </div>
+            </fieldset>
         </div>
     )
 }
