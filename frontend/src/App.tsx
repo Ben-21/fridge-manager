313eb04754ad9142f19154754cf6e07ab5eb5624
Ben-21 <bs@schaefer-inet.de>
2023-10-08 12:44:16 +0200

    build ItemCard.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,28 @@
 import './App.css'
-import AddEditItem from "./pages/AddEditItem.tsx";
 import {ToastContainer} from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import {Item, StockUnit, StorageLocation} from "./models/models.ts";
+import ItemCard from "./components/ItemCard.tsx";
 
 function App() {
+
+    const testItem: Item = {
+        id: "0123",
+        barcode: "0000",
+        name: "Test Item",
+        imageUrl: "https://miro.medium.com/v2/da:true/resize:fit:800/1*nzx2zFmbdZ5xWUQHWohvkw.gif",
+        storageLocation: StorageLocation.FRIDGE,
+        stockAmount: 10,
+        warnStockAmount: 2,
+        stockUnit: StockUnit.PIECE,
+        quantity: "150 g"
+    }
 
 
     return (
         <>
             <ToastContainer/>
-            <AddEditItem/>
+            <ItemCard item={testItem}/>
         </>
     )
 }
