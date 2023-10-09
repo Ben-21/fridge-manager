39122bf8b43d27ee767092f710a6b784a84a408a
Ben-21 <bs@schaefer-inet.de>
2023-10-09 12:34:27 +0200

    add react routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,50 +1,19 @@
 import './App.css'
-import {toast, ToastContainer} from "react-toastify";
+import {ToastContainer} from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
-import {Item} from "./models/models.ts";
-import ItemCard from "./components/ItemCard.tsx";
-import FetchItem from "./components/FetchItem.tsx";
-import {useEffect, useState} from "react";
-import axios from "axios";
-import ListItems from "./components/ListItems.tsx";
+import {Route, Routes} from "react-router-dom";
+import AddEditItem from "./pages/AddEditItem.tsx";
+import Home from "./pages/Home.tsx";
 
 function App() {
-
-    const [barcode, setBarcode] = useState<string>("");
-    const [item, setItem] = useState<Item>();
-    const [items, setItems] = useState<Item[]>([]);
-
-
-    useEffect(()=>{
-        fetchAllItems();
-    },[])
-
-    function fetchAllItems() {
-        axios
-            .get("/api/items")
-            .then((response) => response.data)
-            .then((data) => {
-                setItems(data)
-            })
-            .catch((error) => {
-                toast.error("Something went wrong trying to fetch all items");
-                console.log(error);
-            })
-
-    }
-
-    const fetchItem = (childData: Item) => {
-        setItem(childData);
-    };
-
 
     return (
         <>
             <ToastContainer/>
-            <input name={"barcode"} type={"text"} value={barcode} onChange={(event) => setBarcode(event.target.value)}/>
-            <FetchItem fetchData={fetchItem} barcode={barcode}/>
-            {item && <ItemCard item={item}/>}
-            <ListItems items={items}/>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="add" element={<AddEditItem/>}/>
+            </Routes>
         </>
     )
 }
