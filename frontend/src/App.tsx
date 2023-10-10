b4da9377744e13c44a5562f41172eabdb8988466
Ben-21 <bs@schaefer-inet.de>
2023-10-10 14:29:18 +0200

    rename

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import './App.css'
 import {ToastContainer} from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import {Route, Routes} from "react-router-dom";
-import AddEditItem from "./pages/AddEditItem.tsx";
+import AddItem from "./pages/AddItem.tsx";
 import Home from "./pages/Home.tsx";
 import UpdateItem from "./pages/UpdateItem.tsx";
 
@@ -13,7 +13,7 @@ function App() {
             <ToastContainer/>
             <Routes>
                 <Route path="/" element={<Home/>}/>
-                <Route path="add" element={<AddEditItem/>}/>
+                <Route path="add" element={<AddItem/>}/>
                 <Route path="update/:id" element={<UpdateItem/>}/>
             </Routes>
         </>
