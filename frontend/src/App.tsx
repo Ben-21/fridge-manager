4434f2afd609f71bed6d5bc4b4ae96199e8bd823
Ben-21 <bs@schaefer-inet.de>
2023-10-10 12:55:32 +0200

    add new route for update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import {Route, Routes} from "react-router-dom";
 import AddEditItem from "./pages/AddEditItem.tsx";
 import Home from "./pages/Home.tsx";
+import UpdateItem from "./pages/UpdateItem.tsx";
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="add" element={<AddEditItem/>}/>
+                <Route path="update/:id" element={<UpdateItem/>}/>
             </Routes>
         </>
     )
