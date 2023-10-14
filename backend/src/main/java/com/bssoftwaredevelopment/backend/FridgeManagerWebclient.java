04f6418df31a678a6aec1c1665e75fb0190dd557
Ben-21 <bs@schaefer-inet.de>
2023-10-14 17:15:15 +0200

    add try catch block in webclient off api access. response from off api is 404 when product is not found but is 200 and status = 0 and status_verbose = "no code or invalid code" when code is invalid

--- backend/src/main/java/com/bssoftwaredevelopment/backend/FridgeManagerWebclient.java
+++ backend/src/main/java/com/bssoftwaredevelopment/backend/FridgeManagerWebclient.java
@@ -4,6 +4,7 @@ import com.bssoftwaredevelopment.backend.models.OpenFoodFactsItem;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 import java.util.Objects;
 
@@ -12,14 +13,19 @@ public class FridgeManagerWebclient {
 
     private final WebClient webClient = WebClient.create("https://world.openfoodfacts.org/api/v2/product/");
 
-      public OpenFoodFactsItem getOpenFoodFactsItem(String barcode) {
-        ResponseEntity<OpenFoodFactsItem> responseEntity = webClient.get()
-                .uri(barcode)
-                .retrieve()
-                .toEntity(OpenFoodFactsItem.class)
-                .block();
+    public OpenFoodFactsItem getOpenFoodFactsItem(String barcode) {
+        try {
+            ResponseEntity<OpenFoodFactsItem> responseEntity = webClient.get()
+                    .uri(barcode)
+                    .retrieve()
+                    .toEntity(OpenFoodFactsItem.class)
+                    .block();
 
-        assert responseEntity != null;
-        return Objects.requireNonNull(responseEntity.getBody());
+            assert responseEntity != null;
+            return Objects.requireNonNull(responseEntity.getBody());
+
+        } catch (WebClientResponseException e) {
+            return new OpenFoodFactsItem(barcode, null, 0, "product not found");
+        }
     }
 }
