e6979abb0d2f2d300aa6719bcec2082d547cc0c3
Ben-21 <bs@schaefer-inet.de>
2023-10-18 11:47:41 +0200

    add cloudinary config

---
+++ backend/src/main/java/com/bssoftwaredevelopment/backend/imageupload/BeanConfigCloudinary.java
@@ -0,0 +1,23 @@
+package com.bssoftwaredevelopment.backend.imageupload;
+
+import com.cloudinary.Cloudinary;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class BeanConfigCloudinary {
+    private static final String CLOUD_NAME = "dfzzbhu3x";
+    private static final String API_KEY = "399958539413299";
+    private static final String API_SECRET = System.getenv("CLOUDINARY_SECRET");
+    @Bean
+    public Cloudinary createCloudinary() {
+        Map<String, String> config = new HashMap<>();
+        config.put("cloud_name",CLOUD_NAME);
+        config.put("api_key",API_KEY);
+        config.put("api_secret",API_SECRET);
+        return new Cloudinary(config);
+    }
+}
